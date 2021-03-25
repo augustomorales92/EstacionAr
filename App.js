7f229f61d7f8315568610abf383e5496e6864327
augustomorales92 <augustomorales92@gmail.com>
2021-03-25 11:16:25 -0300

    esqueleto

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style ={styles.letters}> EstacionAr</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
+   
+  },
+  letters:{
+    color:'white',
+    fontSize:40
   },
 });

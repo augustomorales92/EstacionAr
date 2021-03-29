fc59da6598871026e59bae2fdad23ad0b253e1d5
dborgat <dborgat@gmail.com>
2021-03-29 20:25:33 -0300

    cambios con marian redux

--- components/clock/clock.js
+++ components/clock/clock.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { format } from "./format";
-<<<<<<< HEAD
-import { SafeAreaView, Text, View } from "react-native";
+import { SafeAreaView, View, Text } from "react-native";
 import { styles } from "./clockStyle";
 
 export default ({ time }) => {
@@ -13,23 +12,3 @@ export default ({ time }) => {
     </SafeAreaView>
   );
 };
-=======
-import { SafeAreaView, View, Text } from 'react-native';
-import { styles } from './clockStyle'
-
-
-
-export default ({ time }) => {
-        return (
-                <SafeAreaView style={styles.container}>
-                        <View>
-                                <Text style={styles.clock}>
-                                        {format(time)}
-                                </Text>
-                        </View>
-                </SafeAreaView>
-
-
-        )
-}
->>>>>>> 8e2b113f653154af670cefc6a618ad432f90fd30

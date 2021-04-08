2016299a8d59477017fdd4c7605de84f10d6ca82
dborgat <dborgat@gmail.com>
2021-04-08 17:11:56 -0300

    dia previo a la demo

--- components/EditUser/EditUser.js
+++ components/EditUser/EditUser.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { View, Alert, SafeAreaView, ActivityIndicator } from "react-native";
+import {
+  View,
+  Alert,
+  SafeAreaView,
+  ActivityIndicator,
+  KeyboardAvoidingView,
+} from "react-native";
 import { Button, Input, Card, Image, Text } from "react-native-elements";
 import { validatePassword } from "../../utils/validations";
 import { getUserInfo } from "../../redux/reducer/userActions";
 import firebase from "../../back/db/firebase";
-import {validateEmail} from '../../utils/validations'
+import { validateEmail } from "../../utils/validations";
 
 import { styles } from "./EditUserStyle";
 
@@ -24,8 +30,9 @@ const EditUser = (props) => {
   });
 
   const editUser = () => {
-    const {name, lastname, email} = input
-    if (name === '' || lastname === '') return props.navigation.navigate("home")
+    const { name, lastname, email } = input;
+    if (name === "" || lastname === "")
+      return props.navigation.navigate("home");
     firebase.db
       .collection("users")
       .doc(userId)
@@ -40,62 +47,69 @@ const EditUser = (props) => {
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Card containerStyle={styles.input}>
-        <Input
-          label="Nombre"
-          name="name"
-          placeholder={userInfo ? userInfo.name : ""}
-          inputStyle={styles.colorInput}
-          onChangeText={(value) => handleChangeText("name", value)}
-        />
-        <Input
-          label="Apellido"
-          name="lastname"
-          placeholder={userInfo ? userInfo.lastname : ""}
-          inputStyle={styles.colorInput}
-          onChangeText={(value) => handleChangeText("lastname", value)}
-        />
-        <Input
-          label="Email"
-          name="email"
-          type="email"
-          value={userInfo ? userInfo.email : ""}
-          disabled={true}
-          inputStyle={styles.colorInput}
-          onChangeText={(value) => handleChangeText("email", value)}
-        />
-      </Card>
-      <View style={styles.fixToText}>
-        <Button
-          buttonStyle={styles.colores}
-          title="Actualizar cambios"
-          onPress={() => {
-            if (window.confirm('Está seguro que desea realizar estos cambios?')) editUser();
-          }}
-        ></Button>
-      </View>
-      <View style={styles.signin}>
-        <Button
-          type="clear"
-          title="¿Desea modificar su contraseña?"
-          titleStyle={styles.clearButton}
-          onPress={() => {
-            props.navigation.navigate("datos login");
-          }}
-        ></Button>
-      </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <SafeAreaView style={styles.container}>
+        <Card containerStyle={styles.input}>
+          <Input
+            label="Nombre"
+            name="name"
+            placeholder={userInfo ? userInfo.name : ""}
+            inputStyle={styles.colorInput}
+            onChangeText={(value) => handleChangeText("name", value)}
+          />
+          <Input
+            label="Apellido"
+            name="lastname"
+            placeholder={userInfo ? userInfo.lastname : ""}
+            inputStyle={styles.colorInput}
+            onChangeText={(value) => handleChangeText("lastname", value)}
+          />
+          <Input
+            label="Email"
+            name="email"
+            type="email"
+            value={userInfo ? userInfo.email : ""}
+            disabled={true}
+            inputStyle={styles.colorInput}
+            onChangeText={(value) => handleChangeText("email", value)}
+          />
+        </Card>
+        <View style={styles.fixToText}>
+          <Button
+            buttonStyle={styles.colores}
+            title="Actualizar cambios"
+            onPress={() => {
+              if (
+                window.confirm("Está seguro que desea realizar estos cambios?")
+              )
+                editUser();
+            }}
+          ></Button>
+        </View>
+        <View style={styles.signin}>
+          <Button
+            type="clear"
+            title="¿Desea modificar su contraseña?"
+            titleStyle={styles.clearButton}
+            onPress={() => {
+              props.navigation.navigate("datos login");
+            }}
+          ></Button>
+        </View>
 
-
-      <View style={styles.imagen}>
-        <Image
-          style={styles.stretch}
-          source={{
-            uri: "https://i.postimg.cc/mrWQN3x1/logo-final-8.png",
-          }}
-        />
-      </View>
-    </SafeAreaView>
+        <View style={styles.imagen}>
+          <Image
+            style={styles.stretch}
+            source={{
+              uri: "https://i.postimg.cc/mrWQN3x1/logo-final-8.png",
+            }}
+          />
+        </View>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 };
 
--- components/SignUp/SignUpStyle.js
+++ components/SignUp/SignUpStyle.js
@@ -1,57 +1,52 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'orange',
-      height: '100%',
-      
-     
-    },
-    
-      stretch: {
-        width: 200,
-        height: 180,
-        resizeMode: 'stretch',
-        
-      },
-      fixToText: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginTop: 20,
-        marginRight:20
-      },
-      imagen:{
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '35%'
-      },
-      labelstyle:{
-        color:'black',
-        textAlign:'center'
-    },
-    formStyle:{
-        margin:20
-    },
-    itemStyle:{
-        marginTop: 30,
-        borderWidth: 1,
-        borderColor: 'black'
-    },
-    colores:{
-      fontSize:20,
-      backgroundColor:'black'
-      },
-    input: {
-      backgroundColor:'black',
-      borderRadius:10,
-      opacity:0.5,
-      marginTop:30
-      },
-      colorInput:{
-        color:'white'
-      }
-   
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "orange",
+    // height: "100%",
+  },
+
+  stretch: {
+    width: 200,
+    height: 180,
+    resizeMode: "stretch",
+  },
+  fixToText: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
+    marginRight: 20,
+  },
+  imagen: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "35%",
+  },
+  labelstyle: {
+    color: "black",
+    textAlign: "center",
+  },
+  formStyle: {
+    margin: 20,
+  },
+  itemStyle: {
+    marginTop: 30,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  colores: {
+    fontSize: 20,
+    backgroundColor: "black",
+  },
+  input: {
+    backgroundColor: "black",
+    borderRadius: 10,
+    opacity: 0.5,
+    marginTop: 0,
+  },
+  colorInput: {
+    color: "black",
+  },
+});
